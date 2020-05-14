f6aea8100dc8cb3e005d1384c9771bb71c571cf8
Jian Ke Yu <jiankeyu@JiandeiMac.local>
2020-05-14 18:53:23 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http')
+
+const homePage = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Node.js 部署上线示例</title>
+    </head>
+    <body>
+      <h1>慕课网 Nodejs 部署发布</h1>
+    </body>
+  </html>
+`
+
+http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end(homePage)
+}).listen(5000, () => {
+  console.log('Server running at 5000')
+})
+
+//ssh -p 39999 rn_manager@47.107.166.91
